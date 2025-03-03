abcf4cf0167bbeb0cd5cbfd14468db0c92cde9d8
Bruno Ortiz <bruno.ortiz_a10@ciencias.unam.mx>
2025-03-03 10:44:54 -0600

    Correciones en el archivo tablasMultiplicar.js

---
+++ Node/03_BASES-NODE/tablasMultiplicar.js
@@ -0,0 +1,29 @@
+const { Console } = require('console');
+const fs = require ('fs');
+
+
+const base = 3;
+
+console.clear();
+
+console.log('====================================================');
+
+console.log(`TABLA DEL ${base}`);
+
+console.log('====================================================');
+
+
+let salida = '';
+for (let i = 1; i <= 10; i++) {
+    salida += `${base} x ${i} = ${base * i} \n`;
+}
+
+console.log(salida)
+
+fs.writeFile(`tabla${base}.txt`, salida, (err) => {
+    if (err) {
+        throw err;
+    }
+
+    console.log(`Archivo tabla${base}.txt creado con exito`);
+})
