854208923ace816d3caed903b07658076e0fb4d3
Bruno Ortiz <bruno.ortiz_a10@ciencias.unam.mx>
2025-03-05 11:24:39 -0600

    Menu en bucle y salida mediante ingreso de opcion

--- Node/04-AGENDA-TAREAS/app.js
+++ Node/04-AGENDA-TAREAS/app.js
@@ -5,9 +5,16 @@ const { mostrarMenu, pausa } = require('./helpers/mensajes');
 console.clear();
 
 const main = async() => {
+
+    let opt = '';
     
-    mostrarMenu();
-    //pausa();
+    do{
+
+        opt = await mostrarMenu();
+        console.log({ opt });
+        if (opt !== '0') await pausa();
+
+    } while( opt !== '0' );
 }
 
 main();
--- Node/04-AGENDA-TAREAS/helpers/mensajes.js
+++ Node/04-AGENDA-TAREAS/helpers/mensajes.js
@@ -1,40 +1,53 @@
 const { magenta } = require('colors');
+const { resolve } = require('path');
 
 require('colors');
 
 const mostrarMenu = () => {
 
-    console.log('============================='.magenta);
-    console.log('    Selecciona una opcion    '.magenta);
-    console.log('============================= \n'.magenta);
+    return new Promise( resolve => {
 
-    console.log(`${'1.'.magenta} Crear tarea`);
-    console.log(`${'2.'.magenta} Listar tareas`);
-    console.log(`${'3.'.magenta} Listar tareas completas`);
-    console.log(`${'4.'.magenta} Listar tareas pendientes`);
-    console.log(`${'5.'.magenta} Completar tarea(s)`);
-    console.log(`${'6.'.magenta} Borrar tarea`);
-    console.log(`${'0.'.magenta} Salir \n`);
+        console.clear();
+
+        console.log('============================='.magenta);
+        console.log('    Selecciona una opcion    '.magenta);
+        console.log('============================= \n'.magenta);
     
-    const readLine = require('readline').createInterface({
-        input: process.stdin,
-        output: process.stdout
-    });
+        console.log(`${'1.'.magenta} Crear tarea`);
+        console.log(`${'2.'.magenta} Listar tareas`);
+        console.log(`${'3.'.magenta} Listar tareas completas`);
+        console.log(`${'4.'.magenta} Listar tareas pendientes`);
+        console.log(`${'5.'.magenta} Completar tarea(s)`);
+        console.log(`${'6.'.magenta} Borrar tarea`);
+        console.log(`${'0.'.magenta} Salir \n`);
+        
+        const readLine = require('readline').createInterface({
+            input: process.stdin,
+            output: process.stdout
+        });
+    
+        readLine.question(`Seleccione una opción: `, (opt) => {
+            readLine.close();
+            resolve(opt);
+        })
 
-    readLine.question(`Seleccione una opción: `, (opt) => {
-        readLine.close();
-    })
+    });
 }
 
 const pausa = () => {
-    const readLine = require('readline').createInterface({
-        input: process.stdin,
-        output: process.stdout
+
+    return new Promise( resolve => {
+        const readLine = require('readline').createInterface({
+            input: process.stdin,
+            output: process.stdout
+        });
+    
+        readLine.question(`Presione ${'Enter'.magenta} para continuar \n`, (opt) => {
+            readLine.close();
+            resolve();
+        })
     });
 
-    readLine.question(`Presione ${'Enter'.magenta} para continuar \n`, (opt) => {
-        readLine.close();
-    })
 }
 
 module.exports = {
