4485c6cf988084225a653282a5786f6ecd20fea4
Bruno Ortiz <bruno.ortiz_a10@ciencias.unam.mx>
2025-03-05 10:14:49 -0600

    Cambio en multiplicar.js para que su salida se guarde en la carpeta salida

--- Node/03_BASES-NODE/helpers/multiplicar.js
+++ Node/03_BASES-NODE/helpers/multiplicar.js
@@ -76,7 +76,7 @@ const crearArchivo = async( base = 5, listar = false, hasta = 10) => {
             console.log(salida)    
         }
     
-        fs.writeFileSync(`tabla${base}.txt`, salida);
+        fs.writeFileSync(`./salida/tabla${base}.txt`, salida);
 
         return `tabla-${ base }.txt`;
     } catch (error) {
---
+++ Node/03_BASES-NODE/salida/info.md
@@ -0,0 +1,3 @@
+# Informacion de la aplicacion.
+
+Esta carpeta contine la salida final despues de la ejecucion de la plicaacion
