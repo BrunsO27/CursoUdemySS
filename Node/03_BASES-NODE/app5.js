716261869cfc7d58d310d8b2eafccb3c9513683c
Bruno Ortiz <bruno.ortiz_a10@ciencias.unam.mx>
2025-03-05 09:43:04 -0600

    Ejercicio de exportacion de configuraciones de yargs

---
+++ Node/03_BASES-NODE/app5.js
@@ -0,0 +1,24 @@
+const {crearArchivo} = require ('./helpers/multiplicar');
+const argv = require ('./config/yargs')
+console.clear();
+
+
+
+
+/* const [,,arg3 = 'base=5'] = process.argv
+const [ , base ] = arg3.split('=');
+ */
+
+
+// const base = 4;
+
+
+
+crearArchivo( argv.b, argv.l )
+    .then( nombreArchivo => console.log(nombreArchivo, 'creado'))
+    .catch(err => console.log(err));
+
+
+
+//console.log(process.argv);
+//console.log(argv);
--- Node/03_BASES-NODE/config/yargs.js
+++ Node/03_BASES-NODE/config/yargs.js
@@ -1,15 +1,16 @@
-const {crearArchivo} = require ('./helpers/multiplicar');
 const argv = require ('yargs')
                      .option('b', {
                         alias: 'base',
                         type: 'number',
-                        demandOption: true
+                        demandOption: true,
+                        description: 'Es el numero base para la tabla de multiplicar'
                      },)
                      .option('l', {
                         alias: 'listar',
                         type: 'boolean',
                         default: false,
-                        demandOption: false
+                        demandOption: false,
+                        describe: 'Lista en consola la tabla de multiplicar'
                      })
                      .check(( argv, options) => {
                         if (isNaN(argv.b)) {
@@ -19,25 +20,5 @@ const argv = require ('yargs')
                      })
                      .argv;
 
-console.clear();
 
-
-
-
-/* const [,,arg3 = 'base=5'] = process.argv
-const [ , base ] = arg3.split('=');
- */
-
-
-// const base = 4;
-
-
-
-crearArchivo( argv.b, argv.l )
-    .then( nombreArchivo => console.log(nombreArchivo, 'creado'))
-    .catch(err => console.log(err));
-
-
-
-//console.log(process.argv);
-//console.log(argv);
+module.exports = argv;
