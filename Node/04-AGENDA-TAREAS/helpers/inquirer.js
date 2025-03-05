356551c183f0494e0f6b8a2f6d9bd0a048d02464
Bruno Ortiz <bruno.ortiz_a10@ciencias.unam.mx>
2025-03-05 11:33:18 -0600

    Avance del menu con inquirer

---
+++ Node/04-AGENDA-TAREAS/helpers/inquirer.js
@@ -0,0 +1,16 @@
+const inquirer = require('inquirer');
+require('colors');
+
+const menuOpts = [
+    
+]
+
+const inquirerMenu = async() => {
+    
+    console.clear();
+    console.log('============================='.magenta);
+    console.log('    Selecciona una opcion    '.magenta);
+    console.log('============================= \n'.magenta);
+
+    await inquirer.promt([]) 
+}
