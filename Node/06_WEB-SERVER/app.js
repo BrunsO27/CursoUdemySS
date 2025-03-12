c5c033978df5ec0b39f6723e8ac26833550f8d82
Bruno Ortiz <bruno.ortiz_a10@ciencias.unam.mx>
2025-03-12 09:25:49 -0600

    Ejercico para servir una página web completa

--- Node/06_WEB-SERVER/app.js
+++ Node/06_WEB-SERVER/app.js
@@ -5,13 +5,20 @@ const port = 8080;
 // Servir contenido estático
 app.use( express.static('public'))
 
+app.get('/index', (req, res) => {
+    res.sendFile(__dirname + '/public/index.html')
+})
 
-app.get('/hola-mundo', (req, res) => {
-    res.send('Hola mundo en su respectivo path')
-}) 
+app.get('/generic', (req, res) => {
+    res.sendFile(__dirname +'/public/generic.html')
+})
+
+app.get('/elements', (req, res) => {
+    res.sendFile(__dirname + '/public/elements.html')
+})
 
 app.get('*', (req, res) => {
-    res.sendFile(__dirname + '/public/404.html');
+    res.sendFile(__dirname + '/public/old/404.html');
 }) 
     
 
