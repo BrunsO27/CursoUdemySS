daf0c3cab7367f980de6bd88058f97bc85497437
Bruno Ortiz <bruno.ortiz_a10@ciencias.unam.mx>
2025-03-19 08:59:10 -0600

    Modelo de usuario terminado

--- Node/07_REST-SERVER/database/config.js
+++ Node/07_REST-SERVER/database/config.js
@@ -2,10 +2,10 @@ const mysql = require('mysql')
 const { Sequelize } = require('sequelize')
 
 const dbCafe = new Sequelize(process.env.DB_NAME, 
-                                   process.env.DB_USER, 
-                                   process.env.DB_PASSWORD,
-                                   {
-    hott: 'localhost',
+                             process.env.DB_USER, 
+                             process.env.DB_PASSWORD,
+                             {
+    host: 'localhost',
     dialect: 'mysql',
 });
 
--- Node/07_REST-SERVER/models/user.js
+++ Node/07_REST-SERVER/models/user.js
@@ -3,7 +3,42 @@ const dbCafe = require('../database/config');
 
 const Usuario = dbCafe.define('usuarios', {
     id: {
-        type: DataTypes.INET,
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
         primaryKey: true,
+    },
+    nombre: {
+        type: DataTypes.STRING(100),
+        allowNull: false,
+    },
+    email: {
+        type: DataTypes.STRING(100),
+        unique: true,
+        allowNull: false,
+    },
+    password: {
+        type: DataTypes.STRING(255),
+        allowNull: false,
+    },
+    google: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: false,
+    },
+    role: {
+        type: DataTypes.ENUM('admin','user'),
+        defaultValue: 'user',
+    },
+    img: {
+        type: DataTypes.STRING,
+    },
+    estado: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: true,
     }
+},
+{
+    freezeTableName: true,
+    timestamps: false,
 })
+
+module.exports = Usuario;
