b451fde96b65dc4b597355351cd817082c80cd3e
Bruno Ortiz <bruno.ortiz_a10@ciencias.unam.mx>
2025-03-26 11:15:53 -0600

    Separacion del HTML y el app.js, ejercicio de eventos

--- Vue/Secciones/02_INTRO_VUE/intro/app.js
+++ Vue/Secciones/02_INTRO_VUE/intro/app.js
@@ -3,26 +3,29 @@ const { createApp, ref} = Vue;
 
 
 const app = createApp({
-    template: `
+    /* template: `
         <h1>{{ message }}</h1>
         <p>- {{ author }}</p>
-    `,
+    `, */
 
     setup() {
         const message = ref("I'm Batman");
-        // const author
-        // Bruce Wayne
-
         const author = ref("Batman");
 
-        setTimeout( () => {
+        const changeQuote = () => {
+            message.value = 'Hola, soy Goku';
+            author.value = 'Goku';
+        }
+
+        /* setTimeout( () => {
             message.value = 'Hola, soy Goku';
             author.value = 'Goku';
             //console.log('Ejecutado');
-        }, 3000)
+        }, 3000) */
         return {
             message,
             author,
+            changeQuote,
         }
     }
 });
