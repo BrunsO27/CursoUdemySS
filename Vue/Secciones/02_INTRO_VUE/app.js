4ce83840e6c8a3046f69f18f18651b2d93ac0b05
Bruno Ortiz <bruno.ortiz_a10@ciencias.unam.mx>
2025-03-26 11:05:34 -0600

    Ejercicio con estados del componente y varialbes reactivas

--- Vue/Secciones/02_INTRO_VUE/app.js
+++ Vue/Secciones/02_INTRO_VUE/app.js
@@ -4,11 +4,27 @@ const { createApp, ref} = Vue;
 
 const app = createApp({
     template: `
-        <h1> Hola mundo </h1>
-        <p> Desde app.js </p>
-    `
+        <h1>{{ message }}</h1>
+        <p>- {{ author }}</p>
+    `,
+
+    setup() {
+        const message = ref("I'm Batman");
+        // const author
+        // Bruce Wayne
+
+        const author = ref("Batman");
+
+        setTimeout( () => {
+            message.value = 'Hola, soy Goku';
+            author.value = 'Goku';
+            //console.log('Ejecutado');
+        }, 3000)
+        return {
+            message,
+            author,
+        }
+    }
 });
 
 app.mount('#myApp')
-
-
