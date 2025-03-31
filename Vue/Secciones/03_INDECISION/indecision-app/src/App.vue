21fa102e7b1be0a1c0b22df9260a46b95bb2a7e8
Bruno Ortiz <bruno.ortiz_a10@ciencias.unam.mx>
2025-03-31 09:07:48 -0600

    Estructuración del chat

--- Vue/Secciones/03_INDECISION/indecision-app/src/App.vue
+++ Vue/Secciones/03_INDECISION/indecision-app/src/App.vue
@@ -1,5 +1,5 @@
 <template>
-    <h1 class="text-3xl font-bold underline">
+    <!-- <h1 class="text-3xl font-bold underline">
     Hello world!
   </h1>
   <hr />
@@ -7,11 +7,15 @@
   <Counter :value="1" />
   <CounterScript :value="2" />
   <CounterScript2 :value="3" />
+ -->
 
+<IndecisionView />
 </template>
 
 <script lang="ts" setup>
-import Counter from './components/Counter.vue';
+/* import Counter from './components/Counter.vue';
 import CounterScript from './components/CounterScript.vue';
-import CounterScript2 from './components/Counter/CounterScript2.vue';
+import CounterScript2 from './components/Counter/CounterScript2.vue'; */
+
+import IndecisionView from './views/indecisionView.vue';
 </script>
