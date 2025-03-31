98929673a5cbb6dcf1647a51c613c601e1a6329f
Bruno Ortiz <bruno.ortiz_a10@ciencias.unam.mx>
2025-03-31 10:47:25 -0600

    Ejercicio con peticiones HTTP

--- Vue/Secciones/03_INDECISION/indecision-app/src/composables/useChat.ts
+++ Vue/Secciones/03_INDECISION/indecision-app/src/composables/useChat.ts
@@ -1,25 +1,50 @@
-import type { ChatMessage } from "@/interfaces/chat-message.interface";
-import { ref } from "vue";
-
+import { sleep } from '@/helpers/sleep';
+import type { ChatMessage } from '@/interfaces/chat-message.interface';
+import type { YesNoResponse } from '@/interfaces/yes-no-response.interface';
+import { ref } from 'vue';
 
 export const useChat = () => {
+  const messages = ref<ChatMessage[]>([]);
 
-    const messages = ref<ChatMessage[]>([]);
-      
-      const onMessage = (text: string) => {
-        messages.value.push({
-          id: new Date().getTime(),
-          itsMine: true,
-          message: text,
-        });
-      }
+  const getHerResponse = async() => {
+    const resp = await fetch('https://yesno.wtf/api');
+    const data = (await resp.json()) as YesNoResponse;
 
-    return {
-        // Properties
-        messages,
+    return data;
+  }
 
-        //methos
-        onMessage,
+  const onMessage = async(text: string) => {
+    if (text.length === 0) {
+      return;
     }
 
-}
+    messages.value.push({
+      id: new Date().getTime(),
+      itsMine: true,
+      message: text,
+    });
+
+    // evaluar si termina con un ?
+    if (!text.endsWith('?')) {
+      return;
+    }
+
+    await sleep(1.5);
+    const { answer, image} = await getHerResponse();
+
+    messages.value.push({
+        id: new Date().getTime(),
+        itsMine: false,
+        message: answer,
+        image: image,
+    });
+  };
+
+  return {
+    // Properties
+    messages,
+
+    //methos
+    onMessage,
+  };
+};
