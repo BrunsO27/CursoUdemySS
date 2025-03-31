5f34666e69dad84be366a4a0ff1251970130dbb4
Bruno Ortiz <bruno.ortiz_a10@ciencias.unam.mx>
2025-03-31 09:27:04 -0600

    Diseño de las burbujas de mensaje

--- Vue/Secciones/03_INDECISION/indecision-app/src/components/chat/ChatMessages.vue
+++ Vue/Secciones/03_INDECISION/indecision-app/src/components/chat/ChatMessages.vue
@@ -2,17 +2,13 @@
   <div class="flex-1 overflow-y-auto p-4">
     <div class="flex flex-col space-y-2">
       <!-- Messages go here -->
-      <!-- Example Message -->
-      <div class="flex justify-end">
-        <div class="bg-blue-200 text-black p-2 rounded-lg max-w-xs">Hey, how's your day going?</div>
-      </div>
-
-      <!-- Example Received Message -->
-      <div class="flex">
-        <div class="bg-gray-300 text-black p-2 rounded-lg max-w-xs">
-          Not too bad, just a bit busy. How about you?
-        </div>
-      </div>
+        <ChatBubble :its-mine="true" message="Hola Mundo"/>
+        <ChatBubble :its-mine="false" message="no" image="https://yesno.wtf/assets/no/11-e6b930256265890554c1464973ebba55.gif"/>
     </div>
   </div>
 </template>
+
+<script setup lang="ts">
+import ChatBubble from './ChatBubble.vue';
+
+</script>
