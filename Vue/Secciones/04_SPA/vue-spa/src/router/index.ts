9fbdacfa314377daa4c4314910b101340055743f
Bruno Ortiz <bruno.ortiz_a10@ciencias.unam.mx>
2025-04-02 11:10:56 -0600

    Uso de rutas padre e hijas

--- Vue/Secciones/04_SPA/vue-spa/src/router/index.ts
+++ Vue/Secciones/04_SPA/vue-spa/src/router/index.ts
@@ -1,31 +1,43 @@
-import HomePage from "@/modules/landing/pages/HomePage.vue";
-import { createRouter, createWebHistory } from "vue-router";
+import HomePage from '@/modules/landing/pages/HomePage.vue'
+import { createRouter, createWebHistory } from 'vue-router'
 
 export const router = createRouter({
-    history: createWebHistory( import.meta.env.BASE_URL),
-    routes: [
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'landing',
+      component: () => import('@/modules/landing/layouts/LandingLayout.vue'),
+      children: [
         {
-            path: '/',
-            name: 'home',
-            component: HomePage,
+          path: '/',
+          name: 'home',
+          component: HomePage,
         },
         {
-            path: '/features',
-            name: 'features',
-            component: () => import('@/modules/landing/pages/FeaturesPages.vue'),
+          path: '/features',
+          name: 'features',
+          component: () => import('@/modules/landing/pages/FeaturesPages.vue'),
         },
         {
-            path: '/pricing',
-            name: 'pricing',
-            component: () => import('@/modules/landing/pages/PricingPage.vue'),
+          path: '/pricing',
+          name: 'pricing',
+          component: () => import('@/modules/landing/pages/PricingPage.vue'),
         },
         {
-            path: '/contact',
-            name: 'contact',
-            component: () => import('@/modules/landing/pages/ContactPage.vue'),
-        }
+          path: '/contact',
+          name: 'contact',
+          component: () => import('@/modules/landing/pages/ContactPage.vue'),
+        },
+      ],
+    },
 
-    ]
-});
+    //Auth
+    {
+      path: '/auth',
+      component: () => import('@/modules/aut/pages/LoginPage.vue'),
+    },
+  ],
+})
 
-export default router;
+export default router
