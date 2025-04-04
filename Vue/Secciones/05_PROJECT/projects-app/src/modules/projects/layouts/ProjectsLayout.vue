7505497fb4563d40e426ec70d3cd4c850d4e7165
Bruno Ortiz <bruno.ortiz_a10@ciencias.unam.mx>
2025-04-04 09:18:15 -0600

    Creación del proyecto, avence en algunos componentes

---
+++ Vue/Secciones/05_PROJECT/projects-app/src/modules/projects/layouts/ProjectsLayout.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="flex flex-col">
+    <!-- Top menu-->
+    <TopMenu />
+    <main>
+      <!-- Side menu -->
+       <SideMenu/>
+      <!-- RouterView -->
+      <router-view />
+    </main>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import TopMenu from '@/modules/common/components/TopMenu.vue';
+import SideMenu from '../components/SideMenu.vue';
+</script>
