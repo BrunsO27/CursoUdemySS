813cb106e0cb84f96420d55a2c3d6b9ac8946ff7
Bruno Ortiz <bruno.ortiz_a10@ciencias.unam.mx>
2025-04-07 08:59:59 -0600

    Ejercicio de un CustomModal

--- Vue/Secciones/05_PROJECT/projects-app/src/modules/projects/views/ProjectsView.vue
+++ Vue/Secciones/05_PROJECT/projects-app/src/modules/projects/views/ProjectsView.vue
@@ -21,28 +21,53 @@
     </table>
   </div>
 
-  <InputModal 
+  <InputModal
     :open="modalOpen"
     @close="modalOpen = false"
     @value="onNewValue"
     placeholder="Ingrese el nombre del proyecto"
     title="Nuevo proyecto"
-    subTitle="Dale un nombre a tu nuevo proyecto"/>
+    subTitle="Dale un nombre a tu nuevo proyecto"
+  />
+
+  <CustomModal :open="customModalOpen">
+    <template #header>
+      <h1 class="text-3xl">Titulo del modal</h1>
+    </template>
+
+    <template #body>
+      <h1>Fugiat deserunt ut et enim commodo ea minim in non minim.</h1>
+    </template>
+
+    <template #footer>
+      <div class="flex justify-end">
+        <button @click="customModalOpen = false" class="btn mr-4">Close</button>
+        <button @click="customModalOpen = false" class="btn btn-primary">Aceptar</button>
+      </div>
+    </template>
+  </CustomModal>
 
   <FabButton @click="modalOpen = true" position="bottom-right">
     <AddCircle />
   </FabButton>
+
+  <FabButton @click="customModalOpen = true" position="bottom-left">
+    <ModalIcon />
+  </FabButton>
 </template>
 
 <script setup lang="ts">
+import CustomModal from '@/modules/common/components/CustomModal.vue';
 import FabButton from '@/modules/common/components/FabButton.vue';
 import InputModal from '@/modules/common/components/InputModal.vue';
 import AddCircle from '@/modules/common/icons/AddCircle.vue';
+import ModalIcon from '@/modules/common/icons/ModalIcon.vue';
 import { ref } from 'vue';
 
 const modalOpen = ref(false);
+const customModalOpen = ref(false);
 
-const onNewValue = ( projectNme: string) => {
-  console.log({projectNme});
-}
+const onNewValue = (projectNme: string) => {
+  console.log({ projectNme });
+};
 </script>
