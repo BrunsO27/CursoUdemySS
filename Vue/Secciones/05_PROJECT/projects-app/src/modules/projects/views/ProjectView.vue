61d3817face8e1aa17d78d6b30e4fb439213ccc4
Bruno Ortiz <bruno.ortiz_a10@ciencias.unam.mx>
2025-04-07 21:57:52 -0600

    Redireccion si no existe proyecto, avance en la pantalla de tareas dentro de un proyecto

--- Vue/Secciones/05_PROJECT/projects-app/src/modules/projects/views/ProjectView.vue
+++ Vue/Secciones/05_PROJECT/projects-app/src/modules/projects/views/ProjectView.vue
@@ -1,5 +1,43 @@
 <template>
-  <BreadCrumbs :name="project?.name ?? 'No name'" />
+  <div class="w-full">
+    <section class="m-2">
+      <BreadCrumbs :name="project?.name ?? 'No name'" />
+    </section>
+
+    <section>
+      <div class="overflow-x-auto">
+        <table class="table">
+          <!-- head -->
+          <thead>
+            <tr>
+              <th class="w-14">Completada</th>
+              <th>Tarea</th>
+              <th>Completada En</th>
+            </tr>
+          </thead>
+          <tbody>
+            <!-- row 2 -->
+            <tr class="hover:bg-base-300">
+              <th>2</th>
+              <td>Hart Hagerty</td>
+              <td>Desktop Support Technician</td>
+            </tr>
+
+            <tr class="hover:bg-base-300">
+              <th></th>
+              <td>
+                <input
+                  type="text"
+                  class="input input-primary w-full opacity-60 transition-all hover:opacity-100 focus:opacity-100"
+                  placeholder="Nueva tarea"
+                />
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </section>
+  </div>
 </template>
 
 <script lang="ts" setup>
@@ -7,11 +45,13 @@ import BreadCrumbs from '@/modules/common/components/BreadCrumbs.vue';
 import { useProjectsStore } from '../stores/projects.store';
 import { ref, watch } from 'vue';
 import type { Project } from '../interfaces/project.interface';
+import { useRouter } from 'vue-router';
 
 interface Props {
   id: string;
 }
 
+const router = useRouter();
 const props = defineProps<Props>();
 const projectStore = useProjectsStore();
 const project = ref<Project | null>();
@@ -22,6 +62,9 @@ watch(
   () => props.id,
   () => {
     project.value = projectStore.projectsList.find((project) => project.id === props.id);
+    if (!project.value) {
+      router.replace('/');
+    }
   },
   {
     immediate: true,
