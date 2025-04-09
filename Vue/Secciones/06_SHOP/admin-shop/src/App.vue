77a2ac46efcb7e432bf8ea18088363d2e1d74984
Bruno Ortiz <bruno.ortiz_a10@ciencias.unam.mx>
2025-04-09 10:47:35 -0600

    Separación del layout en diferentes modulos y paginas

--- Vue/Secciones/06_SHOP/admin-shop/src/App.vue
+++ Vue/Secciones/06_SHOP/admin-shop/src/App.vue
@@ -1,5 +1,7 @@
-import { RouterView } from 'vue-router';
 <template>
-  <h1 class="text-3xl font-bold underline">Hello world!</h1>
   <RouterView />
 </template>
+
+<script lang="ts" setup>
+import { RouterView } from 'vue-router';
+</script>
