05a4b77046450332c29735548d92265e67495634
Bruno Ortiz <bruno.ortiz_a10@ciencias.unam.mx>
2025-04-10 10:32:45 -0600

    Instalación y ejercicio de TanStack Query - useQuery

--- Vue/Secciones/06_SHOP/admin-shop/src/App.vue
+++ Vue/Secciones/06_SHOP/admin-shop/src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <RouterView />
+  <VueQueryDevtools />
 </template>
 
 <script lang="ts" setup>
 import { RouterView } from 'vue-router';
+import { VueQueryDevtools } from '@tanstack/vue-query-devtools';
 </script>
