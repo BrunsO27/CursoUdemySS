d813f469bc6d05ddc04303c498c7e803d64eac0f
Bruno Ortiz <bruno.ortiz_a10@ciencias.unam.mx>
2025-04-11 08:23:16 -0600

    Render de productos terminados

--- Vue/Secciones/06_SHOP/admin-shop/src/modules/products/components/ProductCard.vue
+++ Vue/Secciones/06_SHOP/admin-shop/src/modules/products/components/ProductCard.vue
@@ -4,10 +4,7 @@
   >
     <a href="#">
       <div class="relative flex items-end overflow-hidden rounded-xl">
-        <img
-          src="https://images.unsplash.com/photo-1515955656352-a1fa3ffcd111?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
-          alt="Hotel Photo"
-        />
+        <img :src="product.images[0]" :alt="product.title" />
         <div
           class="flex items-center space-x-1.5 rounded-lg bg-blue-500 px-4 py-1.5 text-white duration-100 hover:bg-blue-600"
         >
@@ -31,11 +28,11 @@
       </div>
 
       <div class="mt-1 p-2">
-        <h2 class="text-slate-700">Adobe Photoshop CC 2022</h2>
-        <p class="mt-1 text-sm text-slate-400">Lisbon, Portugal</p>
+        <h2 class="text-slate-700">{{ product.title }}</h2>
+        <p class="mt-1 text-sm text-slate-400 capitalize">[{{ product.gender }}]</p>
 
         <div class="mt-3 flex items-end justify-between">
-          <p class="text-lg font-bold text-blue-500">$850</p>
+          <p class="text-lg font-bold text-blue-500">${{ product.price }}</p>
 
           <div
             class="flex items-center space-x-1.5 rounded-lg bg-blue-500 px-4 py-1.5 text-white duration-100 hover:bg-blue-600"
