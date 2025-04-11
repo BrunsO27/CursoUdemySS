c72b1ab810477d7868ab0e0408f119f4e6ae9d2d
Bruno Ortiz <bruno.ortiz_a10@ciencias.unam.mx>
2025-04-11 11:07:43 -0600

    Creación de AuthStore con Pinia

---
+++ Vue/Secciones/06_SHOP/admin-shop/src/modules/auth/stores/auth.store.ts
@@ -0,0 +1,53 @@
+import { ref, computed } from 'vue';
+import { defineStore } from 'pinia';
+import { AuthStatus, type User } from '../interfaces';
+import { loginAction } from '../actions';
+
+export const useAuthStore = defineStore('auth', () => {
+  // Authenticated, unAuthebticate, Checking
+  const authStatus = ref(AuthStatus.Checking);
+  const user = ref<User | undefined>();
+  const token = ref('');
+
+  const login = async (email: string, password: string) => {
+    try {
+      const loginResponse = await loginAction(email, password);
+      if (!loginResponse.ok) {
+        return false;
+      }
+
+      user.value = loginResponse.user;
+      token.value = loginResponse.token;
+      authStatus.value = AuthStatus.Authenticated;
+      return true;
+    } catch (error) {
+      console.log(error);
+      return logout();
+    }
+  };
+
+  const logout = () => {
+    authStatus.value = AuthStatus.Unauthenticated;
+    user.value = undefined;
+    token.value = '';
+    return false;
+  };
+
+  return {
+    user,
+    token,
+    authStatus,
+
+    // Getters
+    isChecking: computed(() => authStatus.value === AuthStatus.Checking),
+    isAuthenticated: computed(() => authStatus.value === AuthStatus.Authenticated),
+    isUnauthenticated: computed(() => authStatus.value === AuthStatus.Unauthenticated),
+
+    // Todo: getter para saber si es admin o no
+
+    username: computed(() => user.value?.fullName),
+
+    // Actions
+    login,
+  };
+});
